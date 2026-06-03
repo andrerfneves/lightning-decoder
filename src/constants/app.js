8f012ae0c8d2fe052b8ee18a6e2ea39ada5f8b81
André Neves <andrerfneves@protonmail.com>
2026-06-03 22:04:12 +0000

    chore(deps): remove dead code from constants
    - Remove src/constants/app.js which is not imported anywhere
    - Remove buggy lowercase timeExpireDate export from src/constants/keys.js
      (value was 'min_final_cltv_expiry' which is wrong; unused in codebase)

--- src/constants/app.js
+++
@@ -1,6 +0,0 @@
-// App-Wide Constants
-export const APP_NAME = 'Lightning Decoder';
-export const APP_TAGLINE = 'Decode Lightning Network Requests';
-export const APP_SUBTAGLINE = 'Lightning Address, LNURL, BOLT11, and BOLT12'
-export const APP_INPUT_PLACEHOLDER = 'Enter Invoice';
-export const APP_GITHUB = 'https://github.com/andrerfneves/lightning-decoder';
